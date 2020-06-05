a42e5a0b8b07c6f3da6ea685ed9fdebbc0e6a49f
abbodialbsheralahmad <62264761+abbodialbsheralahmad@users.noreply.github.com>
2020-06-05 16:44:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ apt install python -y
 apt install pip -y
 apt install wordlist -y
 pip install wordlist
-
+apt install perl -y
+apt install crunch -y
 
 # For termux
 
@@ -20,7 +21,8 @@ pkg install python -y
 pkg install pip -y
 pkg install wordlist -y
 pip install wordlist
-
+pkg install perl -y
+pkg install crunch -y 
 
 echo ""
 echo ""
